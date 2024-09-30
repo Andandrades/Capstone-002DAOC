2f66860dd2a4390fdc21bcc855795d8346d698c5
MatiasCamposB <mat.camposb@duocuc.cl>
2024-09-29 21:33:56 -0300

    Variables de entorno

--- soldado-gym/src/index.js
+++ soldado-gym/src/index.js
@@ -2,11 +2,12 @@
 const express =require('express');
 const morgan = require('morgan');
 
+
+
 const rolesRoutes = require('./routes/roles.routes');
 const usersRoutes = require('./routes/users.routes')
 const rolesExercise = require('./routes/exercise.Routes');
 const ExercisesRecords = require('./routes/exerciseHistory.Routes');
-
 
 
 const app = express();
