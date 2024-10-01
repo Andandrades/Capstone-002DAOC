afdb01eb7bafddd529e46f3b5bb7ec48298149e2
MatiasCamposB <mat.camposb@duocuc.cl>
2024-09-30 21:39:56 -0300

    Update index.js

--- soldado-gym/src/index.js
+++ soldado-gym/src/index.js
@@ -8,15 +8,8 @@ const rolesRoutes = require('./routes/roles.routes');
 const usersRoutes = require('./routes/users.routes')
 const rolesExercise = require('./routes/exercise.Routes');
 const ExercisesRecords = require('./routes/exerciseHistory.Routes');
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 const nutriScheduleRoutes = require('./routes/nutri_schedule.routes')
-=======
->>>>>>> Stashed changes
 const schedule_classes = require('./routes/scheduleClases.routes');
-
->>>>>>> a79c53f4d8f8141f618939a4768f32e7a41424e1
 
 const app = express();
 
@@ -28,15 +21,9 @@ app.use(rolesRoutes);
 app.use(usersRoutes);
 app.use(rolesExercise);
 app.use(ExercisesRecords);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-app.use(nutriScheduleRoutes)
-=======
->>>>>>> Stashed changes
+app.use(nutriScheduleRoutes) 
 app.use(schedule_classes);
 
->>>>>>> a79c53f4d8f8141f618939a4768f32e7a41424e1
 
 app.listen(3000)
 console.log('Sever port: 3000')
