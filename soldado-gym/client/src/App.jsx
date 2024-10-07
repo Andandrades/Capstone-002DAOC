d7711516f1ba579c7e7befe3e4465b3fcb47575c
Andandrades <and.andrades@duocuc.cl>
2024-10-07 19:59:53 -0300

    creacion de vista mis clases

--- soldado-gym/client/src/App.jsx
+++ soldado-gym/client/src/App.jsx
@@ -8,10 +8,12 @@ import {
 import { RegisterPage } from "./Pages/Register/RegisterPage";
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
 import { LoginPage } from "./Pages/Login/LoginPage";
-import RecoverPage from "./Pages/Recover/RecoverPage"; 
+import RecoverPage from "./Pages/Recover/RecoverPage";
 import { Menu } from "./Pages/Menu/Menu";
 import { ProfilePage } from "./Pages/Profile/ProfilePage";
 import { SchedulePage } from "./Pages/Schedule/SchedulePage";
+import { ClassesPage } from "./Pages/Classes/ClassesPage";
+
 
 
 function App() {
@@ -19,15 +21,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/inicio" element={<Menu />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/recover" element={<RecoverPage />} /> 
-        <Route path="/Profile" element={<ProfilePage />} /> 
-        <Route path="/schedule" element={<SchedulePage />} /> 
-        <Route path="/menu" element={<SchedulePage />} /> 
-        <Route path="/classes" element={<SchedulePage />} /> 
-
+        <Route path="/recover" element={<RecoverPage />} />
+        <Route path="/inicio" element={<Menu />} />
+        <Route path="/schedule" element={<SchedulePage />} />
+        <Route path="/classes" element={<ClassesPage />} />
+        <Route path="/Profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
--- soldado-gym/client/src/Pages/Profile/ProfilePage.jsx
+++ soldado-gym/client/src/Pages/Profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ProfileNavBar } from "../../Components/ProfileNavBar";
+import { UserNavBar } from "../../Components/UserNavBar";
 import LockIcon from '@mui/icons-material/Lock';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import EmailIcon from '@mui/icons-material/Email';
@@ -9,72 +9,76 @@ import "./ProfileStyle.css"
 export const ProfilePage = () => {
   return (
     <>
-    <div className="body">
-      <div className="genericocontainer">
-        <form>
-          <div >
-            <div className="ProfilePhoto">
-              <img src={userIcon} alt="" />
-              <span>nombre de ejemplo</span>
-              <button 
-              type="button" 
-              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              cambiar
-              </button>
-            </div>
-          </div>
-
-          <div className="profileslables">
-
-            <div className="profileLabel">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Correo</label>
-              <div className="relative mt-2">
-                <input type="email" name="email" id="email" autoComplete="email"
-                  className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="Tu correo electrónico" />
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <EmailIcon />
-                </div>
+      <div className="body">
+        <div className="genericocontainer">
+          <form>
+            <div >
+              <div className="ProfilePhoto">
+                <img src={userIcon} alt="" />
+                <span>nombre de ejemplo</span>
+                <button
+                  type="button"
+                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                  cambiar
+                </button>
               </div>
             </div>
 
-            <div className="profileLabel">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Edad</label>
-              <div className="relative mt-2">
-                <input type="email" name="email" id="email" autoComplete="email"
-                  className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="ingresa tu edad" />
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <CalendarMonthIcon />
+            <div className="profileslables">
+
+              <div className="profileLabel">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Correo</label>
+                <div className="relative mt-2">
+                  <input type="email" name="email" id="email" autoComplete="email"
+                    className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Tu correo electrónico" />
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <EmailIcon />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="profileLabel">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Constraseña</label>
-              <div className="relative mt-2">
-                <input type="email" name="email" id="email" autoComplete="email"
-                  className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="ingresa una contraseña" />
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <LockIcon />
+              <div className="profileLabel">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Edad</label>
+                <div className="relative mt-3">
+                  <input type="email" name="email" id="email" autoComplete="email"
+                    className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="ingresa tu edad" />
+                  
+                  
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <CalendarMonthIcon />
+                  </div>
+                  
+                </div>
+                
+              </div>
+
+              <div className="profileLabel">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Constraseña</label>
+                <div className="relative mt-2">
+                  <input type="email" name="email" id="email" autoComplete="email"
+                    className="block w-full pl-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="ingresa una contraseña" />
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <LockIcon />
+                  </div>
                 </div>
               </div>
+
             </div>
 
-          </div>
 
 
-
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
-            <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
+              <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
              focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Confirmar</button>
-          </div>
-        </form>
-      </div>
+            </div>
+          </form>
+        </div>
 
-      <ProfileNavBar />
+        <UserNavBar />
       </div>
     </>
   );
