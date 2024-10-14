9b8b73696b0d78685db040ab7e19509e88d1cd69
Andandrades <and.andrades@duocuc.cl>
2024-10-14 18:32:01 -0300

    creacion de componente nutricion y jsx de endpoints

--- soldado-gym/client/src/Components/Endpoints/Endpoints.jsx
+++ soldado-gym/client/src/Components/Endpoints/Endpoints.jsx
@@ -1,6 +1,29 @@
-export const RUTA = {
-    PLANS: "/plans",
-    USERS: "/users",
-    Admin: "/"
+const URL = "http://localhost:3000";
+
+export const obtenerPlanes = async () => {
+    try {
+        const response = await fetch(`${URL}/plans`);
+        if (!response.ok) {
+            throw new Error(`Error en la petición: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al obtener los planes:", error);
+        throw error;
+    }
 };
 
+export const obtenerNutri = async () => {
+    try {
+        const response = await fetch(`${URL}/plans`);
+        if (!response.ok) {
+            throw new Error(`Error en la petición: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al obtener los planes:", error);
+        throw error;
+    }
+};
