813035e491fbad8ab02730bd2187691ca326ae07
TDK213 <tomas.osorio.276@gmail.com>
2024-11-04 14:24:44 -0300

    Actualizacion de rama

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Pages/Admin/AdminUsersManagement/AdminUsersManagement.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Pages/Admin/AdminUsersManagement/AdminUsersManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AdminUsersManagement.css';
 import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
@@ -6,11 +6,12 @@ import SportsIcon from '@mui/icons-material/Sports';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import { NavBarAdmin } from '../../../Components/NavBarAdmin';
+import axios from 'axios';
 
 const AdminUsersManagement = () => {
-  const [clients, setClients] = useState(['Cliente 1', 'Cliente 2', 'Cliente 3']);
-  const [nutritionists, setNutritionists] = useState(['Nutricionista 1', 'Nutricionista 2']);
-  const [trainers, setTrainers] = useState(['Entrenador 1', 'Entrenador 2', 'Entrenador 3']);
+  const [clients, setClients] = useState([]);
+  const [nutritionists, setNutritionists] = useState([]);
+  const [trainers, setTrainers] = useState([]);
 
   const [showClients, setShowClients] = useState(false);
   const [showNutritionists, setShowNutritionists] = useState(false);
@@ -23,10 +24,27 @@ const AdminUsersManagement = () => {
   });
 
   const [showPopup, setShowPopup] = useState(false);
-  const [showAddPopup, setShowAddPopup] = useState(false); // agregar usuario
+  const [showAddPopup, setShowAddPopup] = useState(false);
+  const [showEditPopup, setShowEditPopup] = useState(false); 
   const [selectedUser, setSelectedUser] = useState('');
   const [userType, setUserType] = useState('');
-  const [newUserName, setNewUserName] = useState(''); // usuario Nuevos
+  const [newUserName, setNewUserName] = useState('');
+  const [editUserName, setEditUserName] = useState('');
+
+  useEffect(() => {
+    const fetchUsersByRole = async () => {
+      try {
+        const response = await axios.get('/api/usersByRole');
+        setClients(response.data.clients.map(user => user.name));
+        setNutritionists(response.data.nutritionists.map(user => user.name));
+        setTrainers(response.data.trainers.map(user => user.name));
+      } catch (error) {
+        console.error('Error al obtener los usuarios por rol:', error);
+      }
+    };
+
+    fetchUsersByRole();
+  }, []);
 
   const handleDeleteClick = (user, type) => {
     setSelectedUser(user);
@@ -43,11 +61,14 @@ const AdminUsersManagement = () => {
     setShowPopup(false);
   };
 
-  const toggleDropdown = (userType, index) => {
-    setShowDropdowns(prevState => ({
-      ...prevState,
-      [userType]: prevState[userType] === index ? null : index,
-    }));
+  const toggleDropdown = (user, type) => {
+    if (selectedUser === user) {
+      setSelectedUser(null);
+      setUserType('');
+    } else {
+      setSelectedUser(user);
+      setUserType(type);
+    }
   };
 
   const handleAddUser = (type) => {
@@ -67,8 +88,27 @@ const AdminUsersManagement = () => {
     setShowAddPopup(false);
   };
 
+  const handleEditClick = (user, type) => {
+    setSelectedUser(user);
+    setEditUserName(user);
+    setUserType(type); 
+    setShowEditPopup(true); 
+  };
+
+  const confirmEditUser = () => {
+    if (userType === 'client') {
+      setClients(clients.map(client => (client === selectedUser ? editUserName : client)));
+    } else if (userType === 'nutritionist') {
+      setNutritionists(nutritionists.map(nutritionist => (nutritionist === selectedUser ? editUserName : nutritionist)));
+    } else if (userType === 'trainer') {
+      setTrainers(trainers.map(trainer => (trainer === selectedUser ? editUserName : trainer)));
+    }
+    setShowEditPopup(false); 
+  };
+
   return (
     <>
+      <NavBarAdmin />
       <div className="roles-container">
         <h1 className="roles-title">Administrar roles</h1>
 
@@ -85,12 +125,12 @@ const AdminUsersManagement = () => {
                 <li key={index} className="roles-listItem">
                   <span>{client}</span>
                   <div className="dropdown">
-                    <button className="dropdown-button" onClick={() => toggleDropdown('clients', index)}>
+                    <button className="dropdown-button" onClick={() => toggleDropdown(client, 'client')}>
                       <KeyboardDoubleArrowDownIcon />
                     </button>
-                    {showDropdowns.clients === index && (
+                    {selectedUser === client && (
                       <div className="dropdown-content">
-                        <button className="edit-button" onClick={() => alert('Editar funcionalidad en desarrollo')}>Editar</button>
+                        <button className="edit-button" onClick={() => handleEditClick(client, 'client')}>Editar</button>
                       </div>
                     )}
                   </div>
@@ -113,13 +153,13 @@ const AdminUsersManagement = () => {
                 <li key={index} className="roles-listItem">
                   <span>{nutritionist}</span>
                   <div className="dropdown">
-                    <button className="dropdown-button" onClick={() => toggleDropdown('nutritionists', index)}>
+                    <button className="dropdown-button" onClick={() => toggleDropdown(nutritionist, 'nutritionist')}>
                       <KeyboardDoubleArrowDownIcon />
                     </button>
-                    {showDropdowns.nutritionists === index && (
+                    {selectedUser === nutritionist && (
                       <div className="dropdown-content">
                         <button className="delete-button" onClick={() => handleDeleteClick(nutritionist, 'nutritionist')}>Borrar</button>
-                        <button className="edit-button" onClick={() => alert('Editar funcionalidad en desarrollo')}>Editar</button>
+                        <button className="edit-button" onClick={() => handleEditClick(nutritionist, 'nutritionist')}>Editar</button>
                       </div>
                     )}
                   </div>
@@ -142,13 +182,13 @@ const AdminUsersManagement = () => {
                 <li key={index} className="roles-listItem">
                   <span>{trainer}</span>
                   <div className="dropdown">
-                    <button className="dropdown-button" onClick={() => toggleDropdown('trainers', index)}>
+                    <button className="dropdown-button" onClick={() => toggleDropdown(trainer, 'trainer')}>
                       <KeyboardDoubleArrowDownIcon />
-                    </button>
-                    {showDropdowns.trainers === index && (
+                    </button> 
+                    {selectedUser === trainer && (
                       <div className="dropdown-content">
                         <button className="delete-button" onClick={() => handleDeleteClick(trainer, 'trainer')}>Borrar</button>
-                        <button className="edit-button" onClick={() => alert('Editar funcionalidad en desarrollo')}>Editar</button>
+                        <button className="edit-button" onClick={() => handleEditClick(trainer, 'trainer')}>Editar</button>
                       </div>
                     )}
                   </div>
@@ -174,12 +214,12 @@ const AdminUsersManagement = () => {
       {showAddPopup && (
         <div className="popup-overlay">
           <div className="popup">
-            <p>Agregar nuevo {userType === 'client' ? 'cliente' : userType === 'nutritionist' ? 'nutricionista' : 'entrenador'}</p>
+            <p>Agregar nuevo {userType}</p>
             <input
               type="text"
-              placeholder="Nombre del nuevo usuario"
               value={newUserName}
               onChange={(e) => setNewUserName(e.target.value)}
+              placeholder="Nombre del nuevo usuario"
             />
             <div className="popup-buttons">
               <button className="popup-button confirm-button" onClick={confirmAddUser}>Agregar</button>
@@ -189,10 +229,25 @@ const AdminUsersManagement = () => {
         </div>
       )}
 
-      <NavBarAdmin /> 
+      {showEditPopup && ( // Popup de edición
+        <div className="popup-overlay">
+          <div className="popup">
+            <p>Editar {userType}</p>
+            <input
+              type="text"
+              value={editUserName}
+              onChange={(e) => setEditUserName(e.target.value)}
+              placeholder="Nuevo nombre"
+            />
+            <div className="popup-buttons">
+              <button className="popup-button confirm-button" onClick={confirmEditUser}>Confirmar</button>
+              <button className="popup-button cancel-button" onClick={() => setShowEditPopup(false)}>Cancelar</button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
 
 export default AdminUsersManagement;
-
