c77333f1eb5486a2bcd4173bef2e5cacd493d974
AndAndrades <and.andrades@duocuc.cl>
2024-11-14 20:15:30 -0300

    cambios registro de clase

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/API/WebPayApi.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/API/WebPayApi.jsx
@@ -2,18 +2,18 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
- 
+
 
 export const iniciarTransaccion = async (props) => {
-    const { amount, name, userId,id } = props;
+    const { amount, name, userId, id } = props;
     console.log(userId)
     try {
         const response = await axios.post("http://localhost:3000/iniciar-transaccion", {
-            idplan:id,
+            idplan: id,
             amount: amount,
             sessionId: "mi_sesion",
             buyOrder: name,
-            user_id:userId
+            user_id: userId
         });
 
         window.location.href = response.data.url + "?token_ws=" + response.data.token;
@@ -26,18 +26,18 @@ export const ConfirmacionPago = () => {
     const location = useLocation();
     const tokenWs = new URLSearchParams(location.search).get("token_ws");
 
-    useEffect(() => {
-        const confirmarPago = async () => {
-            try {
-                const response = await axios.get("http://localhost:3000/confirmar-pago", {
-                    token_ws: tokenWs
-                });
-                console.log("Pago confirmado:", response.data);
-            } catch (error) {
-                console.error("Error al confirmar el pago:", error);
-            }
-        };
+    const confirmarPago = async () => {
+        try {
+            const response = await axios.get("http://localhost:3000/confirmar-pago", {
+                token_ws: tokenWs
+            });
+            console.log("Pago confirmado:", response.data);
+        } catch (error) {
+            console.error("Error al confirmar el pago:", error);
+        }
+    };
 
+    useEffect(() => {
         confirmarPago();
     }, [tokenWs]);
 
