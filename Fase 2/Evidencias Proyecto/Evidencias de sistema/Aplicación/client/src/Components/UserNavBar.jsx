3fb7ca9ff1d944cc011ce52fc3d94d1dd804ecb3
Andandrades <and.andrades@duocuc.cl>
2024-11-19 20:00:14 -0300

    refactorizacion navbars

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/UserNavBar.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/UserNavBar.jsx
@@ -5,65 +5,60 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import HouseIcon from '@mui/icons-material/House';
 import TodayIcon from '@mui/icons-material/Today';
 import PersonIcon from '@mui/icons-material/Person';
-import './css/ProfileNavBar.css';
 
 export const UserNavBar = () => {
   const navigate = useNavigate();
 
   const goto = (url) => {
-    navigate(`/${url}`); 
+    navigate(`/${url}`);
   };
 
   return (
-    <div className="navbar z-10 fixed bottom-0">
+    <div className="fixed bottom-0 w-full bg-[#1C1C1C] shadow-inner z-10">
       <Disclosure as="nav">
-        <div>
-          <div className="relative flex h-16 items-center">
-            <div className="ButtonNavBar">
-              <button
-                type="button"
-                onClick={() => goto('Inicio')} 
-                className="relative rounded-full text-gray-400 hover:text-white" >
-                <HouseIcon />
-                <h3>Menu</h3>
-              </button>
-            </div>
+        <div className="relative flex items-center justify-around p-3">
+          <div className="flex flex-col items-center text-gray-400 hover:text-white">
+            <button
+              type="button"
+              onClick={() => goto('Inicio')}
+              className="flex flex-col items-center gap-1"
+            >
+              <HouseIcon />
+              <h3 className="text-xs">Menu</h3>
+            </button>
+          </div>
 
-            <div className="ButtonNavBar">
-              <button
-                type="button"
-                onClick={() => goto('schedule')}
-                className="relative rounded-full text-gray-400 hover:text-white"
-              >
-                <TodayIcon />
-                <h3>Agendar</h3>
-              </button>
-            </div>
+          <div className="flex flex-col items-center text-gray-400 hover:text-white">
+            <button
+              type="button"
+              onClick={() => goto('schedule')}
+              className="flex flex-col items-center gap-1"
+            >
+              <TodayIcon />
+              <h3 className="text-xs">Agendar</h3>
+            </button>
+          </div>
 
-            <div className="ButtonNavBar">
-              <button
-                type="button"
-                onClick={() => goto('classes')} 
-                className="relative rounded-full text-gray-400 hover:text-white"
-              >
-                <span className="absolute -inset-1.5" />
-                <FitnessCenterIcon />
-                <h3>Mis clases</h3>
-              </button>
-            </div>
+          <div className="flex flex-col items-center text-gray-400 hover:text-white">
+            <button
+              type="button"
+              onClick={() => goto('classes')}
+              className="flex flex-col items-center gap-1"
+            >
+              <FitnessCenterIcon />
+              <h3 className="text-xs">Mis clases</h3>
+            </button>
+          </div>
 
-            <div className="ButtonNavBar">
-              <button
-                type="button"
-                onClick={() => goto('profile')} 
-                className="relative rounded-full text-gray-400 hover:text-white"
-              >
-                <span className="absolute -inset-1.5" />
-                <PersonIcon />
-                <h3>Perfil</h3>
-              </button>
-            </div>
-            
+          <div className="flex flex-col items-center text-gray-400 hover:text-white">
+            <button
+              type="button"
+              onClick={() => goto('profile')}
+              className="flex flex-col items-center gap-1"
+            >
+              <PersonIcon />
+              <h3 className="text-xs">Perfil</h3>
+            </button>
           </div>
         </div>
       </Disclosure>
