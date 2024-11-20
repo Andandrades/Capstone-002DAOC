bee60072b9d229d89ac3fe58c08dfde151343b65
Andandrades <and.andrades@duocuc.cl>
2024-11-20 19:53:02 -0300

    correcion vista en consultas modal

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/NutriCard.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Components/NutriCard.jsx
@@ -16,8 +16,7 @@ export const NutriCard = ({ name, amount, description, offer_price }) => {
   };
   return (
     <>
-      <div className="flex flex-col items-center justify-between relative text-white py-7 px-6 rounded-md bg-[#1C1C1C] w-[300px] h-[500px]">
-        <div>
+      <div className="flex flex-col items-center justify-between relative text-white py-7 px-6 rounded-md bg-[#1C1C1C] w-[300px] h-[650px]">
           <div className="flex flex-col items-center justify-center text-center">
             <h1 className="text-3xl font-bold ">{name}</h1>
             {offer_price ? (
@@ -37,10 +36,9 @@ export const NutriCard = ({ name, amount, description, offer_price }) => {
             )}
           </div>
           <div className="flex flex-col justify-center items-center font-semibold">
-            <span className="w-60 text-[15px] md:text-sm lg:min-h-25 md:w-72 mt-10">{description}</span>
+            <span className="w-60 text-[15px] md:text-sm lg:min-h-25 md:w-72 mt-2">{description}</span>
           </div>
-        </div>
-        <div className="">
+        <div >
           <button className=" text-base px-4 rounded-full py-2 text-black font-bold  bg-[#0036C1]"
             onClick={NutriInfo}>
             Reservar
