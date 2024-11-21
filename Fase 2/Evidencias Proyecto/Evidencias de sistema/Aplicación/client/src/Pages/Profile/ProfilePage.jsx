a8b9e02df23e8f45e85d36035f90b783796e23bd
Andandrades <and.andrades@duocuc.cl>
2024-11-21 18:28:06 -0300

    avance para el coso

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Pages/Profile/ProfilePage.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/client/src/Pages/Profile/ProfilePage.jsx
@@ -2,7 +2,6 @@ import CreateIcon from '@mui/icons-material/Create';
 import axios from "axios";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ProfileImage } from "../../Components/ProfileImage";
 import { UserNavBar } from "../../Components/UserNavBar";
@@ -13,7 +12,6 @@ const ProfilePage = () => {
   const { register, handleSubmit, formState: { errors }, setError, } = useForm();
   const [showSecurity, setShowSecurity] = useState(false);
   const [refresh, setRefresh] = useState(false);
-  const navigate = useNavigate();
 
   const handleChangeFile = async (e) => {
     e.preventDefault();
@@ -24,7 +22,6 @@ const ProfilePage = () => {
       return;
     }
 
-    // Validación de tipo de archivo
     const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
     if (!allowedTypes.includes(selectedFile.type)) {
       alert("Solo se permiten imágenes (JPG, PNG, GIF).");
@@ -52,8 +49,18 @@ const ProfilePage = () => {
     }
   };
 
+  const ChangePassword = (data) => {
+    console.log("aprete cambiar contraseña")
+    if (data.password !== data.confirmPassword) {
+      return setError("confirmPassword", {
+        type: "manual",
+        message: "Las contraseñas no coinciden.",
+      });
+    }
+  };
 
   const onSubmit = (data) => {
+    console.log("aprete cambiar el de los datos")
     if (data.password !== data.confirmPassword) {
       return setError("confirmPassword", {
         type: "manual",
@@ -66,101 +73,59 @@ const ProfilePage = () => {
     setShowSecurity((prev) => !prev);
   };
 
-  const ProfileForm = ({ userData, register, errors }) => (
-    <div className="flex flex-col justify-center w-[90%]  mx-auto space-y-4">
-      <label htmlFor="email" className="text-sm font-medium text-gray-700">
-        Correo electrónico
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        value={userData.email}
-        {...register("email")}
-        className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
-      />
-      {errors.email && (
-        <span className="text-red-600 text-sm">{errors.email.message}</span>
-      )}
 
-      <label htmlFor="weight" className="text-sm font-medium text-gray-700">
-        Peso
-      </label>
-      <input
-        type="text"
-        name="weight"
-        id="weight"
-        defaultValue={userData.weight}
-        {...register("weight", { required: "El peso es obligatorio" })}
-        className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
-      />
-      {errors.weight && (
-        <span className="text-red-600 text-sm">{errors.weight.message}</span>
-      )}
 
-      <label htmlFor="height" className="text-sm font-medium text-gray-700">
-        Altura
-      </label>
-      <input
-        type="text"
-        name="height"
-        id="height"
-        defaultValue={userData.height}
-        {...register("height", { required: "La altura es obligatoria" })}
-        className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
-      />
-      {errors.height && (
-        <span className="text-red-600 text-sm">{errors.height.message}</span>
-      )}
-    </div>
-  );
-
-  const SecurityForm = ({ register, errors }) => (
-    <div className="mt-4 space-y-4 bg-indigo-50 p-6 rounded-lg shadow-lg">
+  const SecurityForm = ({ register, errors, onSubmit }) => (
+    <div className="mt-4 space-y-4  p-6 rounded-lg shadow-lg">
       <h3 className="text-xl font-semibold text-gray-800">Configuración de Seguridad</h3>
-      <label htmlFor="password" className="text-sm font-medium text-gray-700">
-        Contraseña
-      </label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        autoComplete="new-password"
-        {...register("password", {
-          required: "La contraseña es obligatoria",
-        })}
-        className="block w-full rounded-md py-2 px-4 bg-indigo-100 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600"
-      />
-      {errors.password && (
-        <span className="text-red-600 text-sm">{errors.password.message}</span>
-      )}
-      <label htmlFor="confirmPassword" className="text-sm font-medium text-gray-700">
-        Confirmar contraseña
-      </label>
-      <input
-        type="password"
-        name="confirmPassword"
-        id="confirmPassword"
-        autoComplete="new-password"
-        {...register("confirmPassword", {
-          required: "Debe confirmar la contraseña",
-        })}
-        className="block w-full rounded-md py-2 px-4 bg-indigo-100 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600"
-      />
-      {errors.confirmPassword && (
-        <span className="text-red-600 text-sm">{errors.confirmPassword.message}</span>
-      )}
-      <div className="mt-6 flex items-center justify-around gap-x-6 pb-8">
 
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-        >
-          Cambiar contraseña
-        </button>
-      </div>
+      <form onSubmit={ChangePassword} className="space-y-4">
+        <label htmlFor="password" className="text-sm font-medium text-gray-700">
+          Contraseña
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          autoComplete="new-password"
+          {...register("password", {
+            required: "La contraseña es obligatoria",
+          })}
+          className="block w-full rounded-md py-2 px-4 bg-indigo-100 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600"
+        />
+        {errors.password && (
+          <span className="text-red-600 text-sm">{errors.password.message}</span>
+        )}
+
+        <label htmlFor="confirmPassword" className="text-sm font-medium text-gray-700">
+          Confirmar contraseña
+        </label>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          autoComplete="new-password"
+          {...register("confirmPassword", {
+            required: "Debe confirmar la contraseña",
+          })}
+          className="block w-full rounded-md py-2 px-4 bg-indigo-100 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-600"
+        />
+        {errors.confirmPassword && (
+          <span className="text-red-600 text-sm">{errors.confirmPassword.message}</span>
+        )}
+
+        <div className="mt-6 flex items-center justify-around gap-x-6 pb-8">
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+          >
+            Cambiar contraseña
+          </button>
+        </div>
+      </form>
     </div>
   );
+
 
   return (
     <div className="flex flex-col items-center">
@@ -195,13 +160,55 @@ const ProfilePage = () => {
                 </div>
               </div>
             </div>
-
             <p className="text-gray-900 mt-2 text-lg font-medium text-center">
               {userData.name}
             </p>
+            <div className="flex flex-col justify-center w-[90%]  mx-auto space-y-4">
+              <label htmlFor="email" className="text-sm font-medium text-gray-700">
+                Correo electrónico
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={userData.email}
+                {...register("email")}
+                className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
+              />
+              {errors.email && (
+                <span className="text-red-600 text-sm">{errors.email.message}</span>
+              )}
 
-            <ProfileForm userData={userData} register={register} errors={errors} />
+              <label htmlFor="weight" className="text-sm font-medium text-gray-700">
+                Peso
+              </label>
+              <input
+                type="text"
+                name="weight"
+                id="weight"
+                defaultValue={userData.weight}
+                {...register("weight", { required: "El peso es obligatorio" })}
+                className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
+              />
+              {errors.weight && (
+                <span className="text-red-600 text-sm">{errors.weight.message}</span>
+              )}
 
+              <label htmlFor="height" className="text-sm font-medium text-gray-700">
+                Altura
+              </label>
+              <input
+                type="text"
+                name="height"
+                id="height"
+                defaultValue={userData.height}
+                {...register("height", { required: "La altura es obligatoria" })}
+                className="text-sm font-medium text-gray-700 bg-indigo-100 border-2 p-2 rounded-md"
+              />
+              {errors.height && (
+                <span className="text-red-600 text-sm">{errors.height.message}</span>
+              )}
+            </div>
             <div className="mt-6 flex items-center justify-around gap-x-6 pb-8">
               <button
                 type="submit"
