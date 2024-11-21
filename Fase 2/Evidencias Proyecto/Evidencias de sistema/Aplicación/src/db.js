6ccc6c33991a611b3cb7cf15139eb73c39976b8e
Andandrades <and.andrades@duocuc.cl>
2024-11-21 15:59:28 -0300

    ssl solo en prod

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/src/db.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicación/src/db.js
@@ -2,12 +2,16 @@
 const { Pool } = require('pg');
 const { db } = require('./config');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   user: db.user,
   password: db.password,
   host: db.host,
   port: db.port,
-  database: db.database
+  database: db.database,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+
 });
 
 module.exports = pool;
